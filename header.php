af1bf404a94644a8cac2b39fbe99016fc5d83703
William Okuno <beastyworld@gmail.com>
2014-01-30 19:09:18 -0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,38 @@
+<?php
+echo("<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">
+<div class=\"nav\"><img src=\"/images/logo.png\"></div>
+<nav align=\"center\">
+    <ul>
+      <li><a href=\"/\">Home</a></li>
+      <li><a href=\"/about\">About Us</a>
+        <ul>
+            <li><a href=\"/crew\">Our Crew</a></li>
+            <li><a href=\"/history\">Our History</a></li>
+            <li><a href=\"/vision\">Our Vision</a></li>
+        </ul>
+      </li>
+      <li><a href=\"/products\">Products</a>
+        <ul>
+            <li><a href=\"/games\">Games</a></li>
+            <li><a href=\"/temp/public\">Pixel</a></li>
+            <li><a href=\"/software\">Software</a></li>
+        </ul>
+      </li>
+      <li><a href=\"/projects\">Projects</a>
+        <ul>
+            <li><a href=\"/visites\">Site Visits</a></li>
+            <li><a href=\"/spot\">Spot Welding</a></li>
+            <li><a href=\"/tig\">TiG Welding</a></li>
+            <li><a href=\"/mig\">MiG Welding</a></li>
+            <li><a href=\"/drain\">Drain Service</a></li>
+        </ul>
+      </li>          
+      <li><a href=\"/contact\">Contact</a>
+        <ul>
+            <li><a href=\"/email\">Email</a></li>
+            <li><a href=\"/pigeon\">Carrier Pigeon</a></li>
+        </ul>
+      </li>
+    </ul>
+</nav>");
+?>
