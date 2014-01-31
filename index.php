6298f2065de1c2d08cf74508b24ee6094c15df1a
William Okuno <beastyworld@gmail.com>
2014-01-30 19:08:49 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+include 'header.php';
+
+echo("<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">
+<div id=\"body\" align=\"center\">HELLO WORLD!<div id=\"images\"></div></div>");
+?>
